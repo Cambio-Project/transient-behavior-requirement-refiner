6fb9216599f86a4320e7dcce58910d3fb03d198c
franksn90 <s.frankausowen@googlemail.com>
2023-11-11 09:34:03 +0100

    Add trend operators

--- src/app/shared/enums/logic-operator.ts
+++ src/app/shared/enums/logic-operator.ts
@@ -1,11 +1,17 @@
 export enum LogicOperator {
-	EQUAL = 'equal',
-	SMALLER_EQUAL = 'smallerEqual',
-	SMALLER = 'smaller',
-	BIGGER_EQUAL = 'biggerEqual',
-	BIGGER = 'bigger',
-	//BIGGER_EQUAL = 'greaterEquals',
-	//BIGGER = 'greater',
+  EQUAL = 'equal',
+  SMALLER_EQUAL = 'smallerEqual',
+  SMALLER = 'smaller',
+  BIGGER_EQUAL = 'biggerEqual',
+  BIGGER = 'bigger',
+  UP = 'trendUpward',
+  UP_STRICT = 'trendUpwardStrict',
+  DOWN = 'trendDownward',
+  DOWN_STRICT = 'trendDownwardStrict',
+}
+
+export function requiresComparisonValue(operator: LogicOperator): boolean {
+  return operator == LogicOperator.BIGGER || operator == LogicOperator.BIGGER_EQUAL || operator == LogicOperator.SMALLER || operator == LogicOperator.SMALLER_EQUAL || operator == LogicOperator.EQUAL
 }
 
 export const LOGIC_OPERATOR_OPTIONS = Object.values(LogicOperator);
